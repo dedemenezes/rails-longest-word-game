51f4767935f7ddc4db57667d64c93233656b4511
Andre Menezes <djdedemenezes@gmail.com>
2021-05-25 01:38:09 -0300

    Code looks beautifuuuul

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'time'
 
 class GamesController < ApplicationController
-  
+
   def new
     @start_time = ""
     @letters = []
@@ -13,6 +13,8 @@ class GamesController < ApplicationController
   def score
     @start_time = Time.parse(params[:start])
     @end_time = Time.now
+    @elapsed_time = @end_time - @start_time
+
     grid_word = params[:grid_word].gsub(" ", "")
     attempt = params[:user_word]
     @result = { content: "", score: ""}
@@ -20,23 +22,19 @@ class GamesController < ApplicationController
     if word_exist?(attempt)
       attempt_hash = hash_counter(attempt)
       grid_hash = hash_counter(grid_word)
-      
-      in_the_grid = check_attempt_grid(attempt_hash, grid_hash)
-      elapsed_time = elapsed_time(@start_time, @end_time)
-      
-      results_array = check_result?(in_the_grid, attempt, elapsed_time)
-      @result[:content] = results_array[0]
-      @result[:score] = results_array[1]
+
+      if fail_attempt_grid(attempt_hash, grid_hash).include? false
+        @result[:content] = "Sorry but <strong>#{attempt.upcase}</strong> can't be build out of #{grid_array.join(", ").upcase}"
+        @result[:score] = 0
+      else
+        @result[:content] = "<strong>CONGRATULATIONS!</strong> #{attempt.upcase} is a valid word and all letters are inside the grid"
+        @result[:score] = checking_score(attempt, @elapsed_time)
+      end
     else
       @result[:content] = "Sorry but <strong>#{attempt.upcase}</strong> is not an english word"
-      @result[:score] = 0; 
+      @result[:score] = 0;
     end
-   
     @result
-  end
-
-  def elapsed_time (start_time, end_time)
-    end_time - start_time
   end
 
   def word_exist?(attempt)
@@ -51,12 +49,14 @@ class GamesController < ApplicationController
     letters
   end
 
-  def check_attempt_grid(attempt_hash, grid_hash)
-    in_the_grid = []
-    attempt_hash.map do |k, v| 
-      in_the_grid << (grid_hash[k.to_sym] >= v)
-    end
-    in_the_grid
+  def in_the_grid?(attempt, grid_word)
+    attempt_hash = hash_counter(attempt)
+    grid_hash = hash_counter(grid_word)
+    fail_attempt_grid(attempt_hash, grid_hash).include? false
+  end
+
+  def fail_attempt_grid(attempt_hash, grid_hash)
+    attempt_hash.map { |letter, amount| (grid_hash[letter.to_sym] >= amount) }
   end
 
   def checking_score(attempt, elapsed_time)
@@ -64,17 +64,4 @@ class GamesController < ApplicationController
     time_points = elapsed_time.round(2) * 4
     (word_points / time_points ) * 100
   end
-
-  def check_result?(in_the_grid, attempt, elapsed_time)
-    if in_the_grid.include? false
-      content = "Sorry but <strong>#{attempt.upcase}</strong> can't be build out of #{grid_array.join(", ").upcase}"
-      points = 0
-    else
-      content = "<strong>CONGRATULATIONS!</strong> #{attempt.upcase} is a valid word and all letters are inside the grid"
-      points = checking_score(attempt, elapsed_time)
-    end
-    [content, points]
-  end
-
-
 end
